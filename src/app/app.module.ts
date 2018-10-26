709ed102555bed4710755fca5502687f97fa34d9
Brendan Shea <padma@email.com>
2018-10-26 08:45:01 -0700

    Make Firebase information available in the app.module file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FeaturedArticleComponent } from './featured-article/featured-article.component';
@@ -9,6 +8,16 @@ import { JumboArticleComponent } from './jumbo-article/jumbo-article.component';
 import { FooterComponent } from './footer/footer.component';
 import { FeaturedMovieComponent } from './featured-movie/featured-movie.component';
 
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [

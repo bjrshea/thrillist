44c3e6f9a45177cb620cfa7e0525658137b2fbd7
Brendan Shea <padma@email.com>
2018-10-26 08:39:56 -0700

    Create api-keys file and store Firebase config info in it.

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyD0xzEYahe7SoDW-ZSiYGG9IN4FcOCvrW4",
+  authDomain: "thrillist-17996.firebaseapp.com",
+  databaseURL: "https://thrillist-17996.firebaseio.com",
+  storageBucket: "thrillist-17996.appspot.com",
+  messagingSenderId: "833688271049"
+};

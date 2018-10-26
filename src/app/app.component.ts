e3c7f4374774ef09496116b87e36d9a48c5cd422
Brendan Shea <padma@email.com>
2018-10-26 14:46:18 -0700

    Fix sizing on categories in featured-articles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,6 @@ import { Featured } from './models/featured.model';
 })
 export class AppComponent {
 
-  bigFeaturedArticles: Featured[] = [
-    new Featured('https://www.teamunify.com/pnws2/_images/cust_bg_203_1315415959177.jpg', 'TRAVEL', ' BEST PLACES TO VISIT IN THE PACIFIC NORTHWEST')
-  ]
-
   bigFeaturedMovies: Featured[] = [
     new Featured('https://www.youtube.com/embed/3aIIzSsCjfI', 'FOOD & DRINK', 'Ramen pizza -- really dough?')
   ]

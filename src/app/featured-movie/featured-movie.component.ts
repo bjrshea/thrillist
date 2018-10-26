26c9f7eeb914d568d503ec5c6e34f39df52a2107
Brendan Shea <padma@email.com>
2018-10-26 16:41:58 -0700

    Link featured-movies data to firebase.

--- src/app/featured-movie/featured-movie.component.ts
+++ src/app/featured-movie/featured-movie.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Featured } from '../models/featured.model'
+import { DataService } from '../data.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-featured-movie',
   templateUrl: './featured-movie.component.html',
-  styleUrls: ['./featured-movie.component.css']
+  styleUrls: ['./featured-movie.component.css'],
+  providers: [DataService]
 })
-export class FeaturedMovieComponent {
-  @Input() childBigFeaturedMovies: Featured[];
+export class FeaturedMovieComponent implements OnInit {
+
+  featuredMovies: FirebaseListObservable<any[]>;
+
+  constructor(private dataService: DataService){}
+
+  ngOnInit(){
+    this.featuredMovies = this.dataService.getFeaturedMovieData();
+  }
 }
